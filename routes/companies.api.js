f77cb74e4d5a51ffd58ced41600e1e53450cf532
hoangtan15081995 <98004746+hoangtan15081995@users.noreply.github.com>
2022-04-22 16:41:00 +0700

    update

--- routes/companies.api.js
+++ routes/companies.api.js
@@ -11,17 +11,43 @@ const loadData = () => {
 router.get("/", function (req, res, next) {
   db = loadData();
   let { companies, jobs, ratings } = db;
-  // const city = req.query.city;
-  // if (city === "Miami") {
-  //   const newJobs = jobs.filter((job) => job.city === city);
-  //   return res.send({ newJobs, message: "companies" });
-  // }
-
   const limit = req.query.limit || 20;
   const page = req.query.page || 1;
+  const city = req.query.city;
   const offset = limit * (page - 1);
-  companies = companies.slice(offset, limit * page);
-  return res.send({ jobs, message: "companies" });
+
+  if (city === "Miami") {
+    const newJobs = jobs.filter((job) => job.city === "Miami");
+    const listCompanyId = newJobs.map((job) => job.companyId);
+    const newCompanies = companies.filter((company) =>
+      listCompanyId.includes(company.id)
+    );
+    const number = newCompanies.length;
+    return res.send({ number, newCompanies, message: "jobs" });
+  }
+  if (city === "Miami,New York") {
+    const jobsMiami = jobs.filter((job) => job.city === "Miami");
+    const jobsNewYork = jobs.filter((job) => job.city === "New York");
+
+    const listMiamiCompanyId = jobsMiami.map((job) => job.companyId);
+    const listNewYorkCompanyId = jobsNewYork.map((job) => job.companyId);
+    const newCompanies = companies.filter(
+      (company) =>
+        listMiamiCompanyId.includes(company.id) &
+        listNewYorkCompanyId.includes(company.id)
+    );
+    const number = newCompanies.length;
+    return res.send({ number, newCompanies, message: "jobs" });
+  }
+  if (!city) {
+    companies = companies.slice(offset, limit * page);
+    const number = companies.length;
+    return res.send({ number, companies, message: "companies" });
+  }
+});
+
+router.post("/", function (req, res, next) {
+  console.log("haha");
 });
 
 module.exports = router;
